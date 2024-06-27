b041b452fc2eae5c7d1943c7916a8ee4c9aa43e1
utkarshmishra0104 <162800419+utkarshmishra0104@users.noreply.github.com>
2024-06-27 20:18:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile Menu Toggle
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+});
